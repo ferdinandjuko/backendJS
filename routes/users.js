8666434e33e737e3c71b1b8f878eb33c4efa10d8
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-09 14:09:45 +0300

    router.route('/:id') fns to factory all method use parmas :id

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,15 @@ router.post('/', (req, res) => {
     res.send('Create User');
 });
 
-router.get('/:id', (req, res) => {
-    res.send(`Get User With ID ${req.params.id}`);
-});
+router.route('/:id').
+    get((req, res) => {
+        res.send(`Get User With ID ${req.params.id}`);
+    })
+    .put((req, res) => {
+        res.send(`Update User With ID ${req.params.id}`);
+    })
+    .delete((req, res) => {
+        res.send(`Delete User With ID ${req.params.id}`);
+    });
 
 module.exports = router
