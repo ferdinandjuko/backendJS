8b144ba737abb7994b5104cdf2ccfd4bc3084736
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-10 15:35:32 +0300

    form POST to user route with body.firstName

--- express-init/routes/users.js
+++ express-init/routes/users.js
@@ -7,10 +7,11 @@ router.get('/', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
-    res.send('User New Form');
+    res.render('index', { firstName: 'aware' });
 });
 
 router.post('/', (req, res) => {
+    console.log(req.body.firstName);
     res.send('Create User');
 });
 
