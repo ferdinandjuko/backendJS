76bcbf3f840020a18b87a9158cdfad9c20dedd4f
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-16 17:30:53 +0300

    method get for /api/stuff

--- sell-thing/backend/routes/api/thing.js
+++ sell-thing/backend/routes/api/thing.js
@@ -16,26 +16,12 @@ router.route('/stuff')
             .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
             .catch((error) => res.status(400).json({ error }));
     })
-    .get((req, res, next) => {
-        const stuff = [
-            {
-                _id: 'oeihfzeoi',
-                title: 'Mon premier objet',
-                description: 'Les infos de mon premier objet',
-                imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-                price: 4900,
-                userId: 'qsomihvqios',
-            },
-            {
-                _id: 'oeihfzeomoihi',
-                title: 'Mon deuxième objet',
-                description: 'Les infos de mon deuxième objet',
-                imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-                price: 2900,
-                userId: 'qsomihvqios',
-            },
-        ];
-        res.status(200).json(stuff);
+    .get((req, res) => {
+        Thing.find().
+            then((stuff) => {
+                res.status(200).json(stuff);
+            })
+            .catch((error) => res.status(400).json({ error }));
     })
     ;
 
