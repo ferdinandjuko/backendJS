9fe7db515a7ad70b916bd76279c10911bf267591
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-17 15:48:58 +0300

    get post put delete method for product

---
+++ sell-thing/backend/routes/api/product.js
@@ -0,0 +1,15 @@
+const Product = require('../../models/Product');
+const express = require('express');
+const router = express.Router();
+const productController = require('../../controllers/productController');
+
+router.route('/')
+    .get(productController.getAllProducts)
+    .post(productController.createProduct);
+
+router.route('/:id')
+    .get(productController.getProductById)
+    .put(productController.updateProduct)
+    .delete(productController.deleteProduct);
+
+module.exports = router;
