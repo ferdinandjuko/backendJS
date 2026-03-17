731cbb8825ef46eb84e4db40b1aa40b961f19d70
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-17 15:53:34 +0300

    allow port front 8081

--- sell-thing/backend/config/allowOrigins.js
+++ sell-thing/backend/config/allowOrigins.js
@@ -2,8 +2,10 @@ const allowOrigins = [
     'https://www.yoursite.com',
     'http://localhost:4200',
     'http://127.0.0.1:4200',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://localhost:4201',
+    'http://127.0.0.1:4201',
+    'http://127.0.0.1:8081',
+    'http://localhost:8081'
 ];
 
 module.exports = allowOrigins;
