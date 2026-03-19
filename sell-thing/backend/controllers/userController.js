fc44d3ec653694bcc489bc063af9c4177c0cf965
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-19 11:23:25 +0300

    signup user by await User.create({

--- sell-thing/backend/controllers/userController.js
+++ sell-thing/backend/controllers/userController.js
@@ -1,11 +1,24 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
-export const signup = async (req, res) => {
+const signup = async (req, res) => {
     if (!req.body?.email || !req.body?.password) {
         return res.status(400).json({ message: 'Email and password are required' });
     }
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    try {
+        const result = await User.create({
+            email: req.body.email,
+            password: hashedPassword
+        });
 
-    res.status(201).json({ message: 'User created' });
+        res.status(201).json({ message: 'User created', user: result });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports = {
+    signup
 }
