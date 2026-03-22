4eab9e4478e28e3061370bc9b15c905a3e252bdf
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-22 14:13:22 +0300

    split req.headers.authorization and get token value only

--- sell-thing/backend/middleware/verifyJWT.js
+++ sell-thing/backend/middleware/verifyJWT.js
@@ -3,4 +3,6 @@ const jwt = require('jsonwebtoken');
 exports.exports = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader?.srtatsWith('Bearer ')) res.sendStatus(401); // Unauthorized
+
+    const token = authHeader.split(' ')[1];
 }
