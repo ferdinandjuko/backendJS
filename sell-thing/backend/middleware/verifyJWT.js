65a57d5fe7a28d31fb81737b198d0326763a900c
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-22 14:11:41 +0300

    check if req.headers.authorization has token value inside it

--- sell-thing/backend/middleware/verifyJWT.js
+++ sell-thing/backend/middleware/verifyJWT.js
@@ -2,4 +2,5 @@ const jwt = require('jsonwebtoken');
 
 exports.exports = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader?.srtatsWith('Bearer ')) res.sendStatus(401); // Unauthorized
 }
