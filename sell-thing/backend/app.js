98b8d44589e41d41e24d8d5546e31029667746fe
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-24 15:05:44 +0300

    userRouter handle signup login of User

--- sell-thing/backend/app.js
+++ sell-thing/backend/app.js
@@ -3,8 +3,10 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const corsOption = require('./config/corsOption');
 const connectDB = require('./config/connDB');
+
 const thingRouter = require('./routes/api/thing');
 const productRouter = require('./routes/api/product');
+const userRouter = require('./routes/user');
 
 dotenv.config();
 
@@ -20,5 +22,6 @@ connectDB();
 
 app.use('/api/stuff', thingRouter);
 app.use('/api/products', productRouter);
+app.use('/api/auth', userRouter);
 
 module.exports = app;
