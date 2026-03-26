8d346e6c3da32667d53725d32accc22c4a256fc6
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-26 15:57:53 +0300

    filename attribut to make unique file name to upload

--- sell-thing/backend/middleware/multer-config.js
+++ sell-thing/backend/middleware/multer-config.js
@@ -3,5 +3,6 @@ const multer = require('multer');
 const storage = multer.diskStorage({
     destionation: (req, file, callback) => {
         callback(null, 'images')
-    }
+    },
+    filename:
 })
