5ad5a050b7adbac8cf00793c42dd82fef0abd7a1
rferdinandjuko <rferdinandjuko@gmail.com>
2026-03-26 16:22:05 +0300

    set the name of the file by to make it most unique possible

--- sell-thing/backend/middleware/multer-config.js
+++ sell-thing/backend/middleware/multer-config.js
@@ -12,6 +12,6 @@ const storage = multer.diskStorage({
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
         const extension = MIME_TYPES[file.minetype];
-        callback(null, name + Date.now() + extension)
+        callback(null, name + Date.now() + '.' + extension)
     }
 })
