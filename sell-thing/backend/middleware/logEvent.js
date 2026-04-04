46e454c9ef7e3e5f6d7085223828a7e4ece25a8c
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-04 13:24:59 +0300

    current time with message passed through the logEvents

--- sell-thing/backend/middleware/logEvent.js
+++ sell-thing/backend/middleware/logEvent.js
@@ -3,3 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs/promises')
 
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'dd-MM-yy\tHH:mm:ss')}`;
+}
