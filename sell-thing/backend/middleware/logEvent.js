c3c44afc399e0e1b4e1d45c4ecf604b2a8ef8391
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-07 16:00:47 +0300

    fix: current time with message passed through the logEvents

--- sell-thing/backend/middleware/logEvent.js
+++ sell-thing/backend/middleware/logEvent.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs/promises')
 
-const logEvents = async (message, logName) => {
+exports.logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'dd-MM-yy\tHH:mm:ss')}`;
     const logItem = `${datetime}\t${uuid()}\t${message}\n`;
     console.log(logItem);
