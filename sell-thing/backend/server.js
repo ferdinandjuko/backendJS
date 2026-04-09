0d41fc5a0f7b1ac87176fc2dd97d41736c3927a6
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-09 16:29:32 +0300

    emit mongoose connection once on open event

--- sell-thing/backend/server.js
+++ sell-thing/backend/server.js
@@ -1,10 +1,14 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('connected to MongoDB');
+})
 server.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
 })
