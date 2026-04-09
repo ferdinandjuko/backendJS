dec6de15340d94d883cffc0a0dff82e219f9c2d1
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-09 17:12:10 +0300

    err.stack on console to more views about error

--- sell-thing/backend/middleware/errorHandler.js
+++ sell-thing/backend/middleware/errorHandler.js
@@ -2,4 +2,5 @@ const { logEvents } = require('./logEvents');
 
 exports.errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}`, 'errLog.log');
+    console.log(err.stack);
 }
