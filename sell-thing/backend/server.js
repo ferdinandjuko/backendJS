f5078c32ac7238768e3f52254b305db7d5318cbb
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-09 16:54:24 +0300

    server created from app listen on 3000 PORT

--- sell-thing/backend/server.js
+++ sell-thing/backend/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 mongoose.connection.once('open', () => {
     console.log('connected to MongoDB');
-})
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    server.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
 })
