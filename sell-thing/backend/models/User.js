201edf817729ce4cdce17f160cb565f7a820ee87
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-10 10:34:47 +0300

    default roles 3456 for each user

--- sell-thing/backend/models/User.js
+++ sell-thing/backend/models/User.js
@@ -3,6 +3,12 @@ const { default: uniqueValidator } = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
+    roles: {
+        User: {
+            type: Number,
+            default: 3456
+        }
+    },
     password: { type: String, required: true }
 })
 
