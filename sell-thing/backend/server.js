043aaec06c600d6b62076318ad654c3cfe26f011
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-13 11:54:11 +0300

    import object inside curly bracket to become function

--- sell-thing/backend/server.js
+++ sell-thing/backend/server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
+const { errorHandler } = require('./middleware/errorHandler');
 
 app.set('port', process.env.PORT || 3000);
 
 const server = http.createServer(app);
+
+app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
     console.log('connected to MongoDB');
