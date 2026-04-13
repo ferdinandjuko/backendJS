bda3a615224b5f9966f4c2cffac57b7b14b19aec
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-13 10:47:35 +0300

    send error server from errorHandler of middleware

--- sell-thing/backend/middleware/errorHandler.js
+++ sell-thing/backend/middleware/errorHandler.js
@@ -3,5 +3,6 @@ const { logEvents } = require('./logEvents');
 exports.errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}`, 'errLog.log');
     console.log(err.stack);
+    res.status(500).send(err.message);
     next();
 }
