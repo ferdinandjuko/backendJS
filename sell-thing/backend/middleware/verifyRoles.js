4b99845ca9e0cbe7c323dd266af56ebf1145ec24
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-14 13:39:25 +0300

    find if one of req.auth.roles includes on allowedRoles

--- sell-thing/backend/middleware/verifyRoles.js
+++ sell-thing/backend/middleware/verifyRoles.js
@@ -2,5 +2,6 @@ const verifyRoles = (...allowedRoles) => {
     return (req, res, next) => {
         if (!req.auth.roles) res.sendStatus(401);
         const rolesArray = [...allowedRoles];
+        const result = req.auth.roles.map(role => role.includes(rolesArray)).find(val => val === true);
     }
 }
