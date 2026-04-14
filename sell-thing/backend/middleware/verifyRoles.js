13a900c3d6137421d70efacd07c7de5cfbd3c7cb
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-14 13:35:35 +0300

    allowed roles spread on parameter

--- sell-thing/backend/middleware/verifyRoles.js
+++ sell-thing/backend/middleware/verifyRoles.js
@@ -1,5 +1,6 @@
 const verifyRoles = (...allowedRoles) => {
     return (req, res, next) => {
         if (!req.auth.roles) res.sendStatus(401);
+        const rolesArray = [...allowedRoles];
     }
 }
