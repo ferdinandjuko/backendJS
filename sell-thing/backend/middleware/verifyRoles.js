d7b1d8fb773a807b00a1544150c243aa717309e6
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-15 13:41:41 +0300

    unauthorized if req.auth.roles not includes inside allowedRoles

--- sell-thing/backend/middleware/verifyRoles.js
+++ sell-thing/backend/middleware/verifyRoles.js
@@ -3,6 +3,7 @@ const verifyRoles = (...allowedRoles) => {
         if (!req.auth.roles) res.sendStatus(401);
         const rolesArray = [...allowedRoles];
         const result = req.auth.roles.map(role => role.includes(rolesArray)).find(val => val === true);
+        if (!result) res.sendStatus(401); // Unauthorized
         next();
     }
 }
