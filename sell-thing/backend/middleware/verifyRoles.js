39fbb6bcc871ac82a9bf0327abb2fc6cbaa3c718
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-15 13:40:22 +0300

    middleware if next()

--- sell-thing/backend/middleware/verifyRoles.js
+++ sell-thing/backend/middleware/verifyRoles.js
@@ -3,5 +3,6 @@ const verifyRoles = (...allowedRoles) => {
         if (!req.auth.roles) res.sendStatus(401);
         const rolesArray = [...allowedRoles];
         const result = req.auth.roles.map(role => role.includes(rolesArray)).find(val => val === true);
+        next();
     }
 }
