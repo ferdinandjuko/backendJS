0bd88bf0408e6c89c1c59ed5c659214f197b8d7a
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-15 14:46:16 +0300

    check if roleUser is include inside allowedUser

--- sell-thing/backend/middleware/verifyRoles.js
+++ sell-thing/backend/middleware/verifyRoles.js
@@ -1,9 +1,12 @@
 const verifyRoles = (...allowedRoles) => {
     return (req, res, next) => {
-        if (!req.auth.roles) res.sendStatus(401);
+        if (!req?.auth?.roles) res.sendStatus(401);
         const rolesArray = [...allowedRoles];
-        const result = req.auth.roles.map(role => role.includes(rolesArray)).find(val => val === true);
+        const userRoles = Object.values(req.auth.roles);
+        const result = userRoles.map(role => rolesArray.includes(role)).find(val => val === true);
         if (!result) res.sendStatus(401); // Unauthorized
         next();
     }
 }
+
+module.exports = verifyRoles;
