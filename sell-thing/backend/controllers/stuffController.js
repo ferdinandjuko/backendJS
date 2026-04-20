1c32d6bd1c1932b502ac119da1a517c687ebd11a
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-20 08:15:31 +0300

    feat delete old file on update as well

--- sell-thing/backend/controllers/stuffController.js
+++ sell-thing/backend/controllers/stuffController.js
@@ -64,6 +64,9 @@ const updateStuff = async (req, res) => {
                 const fileName = thing.imageUrl.split('/images/')[1];
                 const filePath = path.join(__dirname, '..', 'images', fileName);
                 if (fs.existsSync(filePath)) {
+                    fs.unlink(filePath, (err) => {
+                        if (err) console.error('Error deleting file:', err);
+                    });
                 }
             }
             thing.title = stuff.title;
