ed789737e8732e256fd9f6088d97c52f270f3d77
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-20 13:22:14 +0300

    connect to database with dotenv.config() fns

--- with-react/server/server.js
+++ with-react/server/server.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDB = require('./config/connDB');
+const corsOptions = require('./config/corsOptions');
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log(`Server Listen on port 4000`);
+connectDB();
+
+mongoose.connection.once('open', () => {
+    console.log(`DB connected successfully`);
+    app.listen(4000, () => {
+        console.log(`Server Listen on port 4000`);
+    })
 })
+
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
