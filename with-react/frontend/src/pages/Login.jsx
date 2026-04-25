996459a560428f4493913b4f3db7cb748a5d713c
rferdinandjuko <rferdinandjuko@gmail.com>
2026-04-25 15:18:54 +0300

    Login component

--- with-react/frontend/src/pages/Login.jsx
+++ with-react/frontend/src/pages/Login.jsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function Login() {
-    return <div>Login</div>
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className='container'>
+            <h2>Register Account</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name='email'
+                        placeholder='Email'
+                        onChange={(e) =>
+                            setValues({ ...values, [e.target.name]: e.target.value })
+                        }
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name='password'
+                        placeholder='Password'
+                        onChange={(e) =>
+                            setValues({ ...values, [e.target.name]: e.target.value })
+                        }
+                    />
+                </div>
+                <button type='submit'>Submit</button>
+                <span>
+                    Already have an account? <Link to='/register'>Register</Link>
+                </span>
+            </form>
+            <ToastContainer />
+        </div>
+    );
 }
